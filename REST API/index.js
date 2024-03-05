ed5aac7039c619cc5caa08ecf90a0edbd8d7d83d
Stefan Dimitrov <115184100+StefanDimitrov04@users.noreply.github.com>
2024-03-05 12:28:58 +0200

    Update index.js

--- REST API/index.js
+++ REST API/index.js
@@ -33,6 +33,6 @@ app.get( '/', (req,res) => {
     res.send('hello world');
 });
 
-app.listen(3000, () => {
+app.listen(process.env.PORT || 3000, () => {
     console.log('app is listening on port 3000');
 })
