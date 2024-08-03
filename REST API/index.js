2a8d84c5c1305493f81ea15d85f2c1f6fa3497f2
StefanDimitrov04 <s.dimitrov2004@gmail.com>
2024-08-03 17:08:54 +0300

    ddd

--- REST API/index.js
+++ REST API/index.js
@@ -6,12 +6,12 @@ const routes = require('./routes');
 
 
 const app = express();
-app.use(cors({
-    origin: 'http://localhost:4200',
-    credentials: true,
-  }));
+// app.use(cors({
+//     origin: 'http://localhost:4200',
+//     credentials: true,
+//   }));
 
-// app.use(cors());
+app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({extended: false}));
 
